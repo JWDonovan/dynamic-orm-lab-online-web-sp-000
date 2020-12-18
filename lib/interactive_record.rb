d16188632f17f0367f12bc2c2fb9f2c2b7e2500c
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-18 19:44:13 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -4,3 +4,34 @@ require 'active_support/inflector'
 class InteractiveRecord
   
 end
+
+# attr_accessor :id, :name, :grade
+# 
+# def initialize(id: nil, name:, grade:)
+#   @id, @name, @grade = id, name, grade
+# end
+# 
+# def self.table_name
+#   self.class.pluralize.downcased
+# end
+# 
+# def self.column_names
+# end
+# 
+# def table_name_for_insert
+# end
+# 
+# def col_names_for_insert
+# end
+# 
+# def values_for_insert
+# end
+# 
+# def save
+# end
+# 
+# def self.find_by_name(name)
+# end
+# 
+# def self.find_by
+# end
